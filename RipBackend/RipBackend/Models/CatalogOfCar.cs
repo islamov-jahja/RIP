e993697ad941d71a39ec95df29c4941367208ec7
islamov-jahja <yahyaislamov15@mail.ru>
2019-10-15 01:53:54 +0300

    сделал изменения

--- RipBackend/RipBackend/Models/CatalogOfCar.cs
+++ RipBackend/RipBackend/Models/CatalogOfCar.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using RipBackend.Models.User;
 
 namespace RipBackend.Models
 {
     public class CatalogOfCar : DbContext
     {
+        public DbSet<User.User> User { get; set; }
+        public DbSet<Reserve.Reserve> Reserve { get; set; }
+        public DbSet<Car.Car> Car { get; set; }
+        public DbSet<Model.Model> Model { get; set; }
+        public DbSet<Carcase.Carcase> Carcase { get; set; }
+        public DbSet<Marka.Marka> Mark { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("");
